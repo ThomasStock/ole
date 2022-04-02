3a6bef2f4317c68000538496c34f86a31f3d1b01
thomasstock <thomas.stock@aprimo.com>
2022-04-02 11:50:29 +0200

    scale

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import useCalculateSize from './useCalculateSize'
 
 // https://www.kirupa.com/canvas/canvas_high_dpi_retina.htm
 
-const map = { width: 800, height: 1200 }
+const map = { width: 800, height: 900 }
+const field = { width: 400, height: 500 }
 
 const App = () => {
 	const canvasRef = useRef<HTMLCanvasElement | null>(null)
 	const { measuredRef, box } = useCalculateSize()
 	const [context, setContext] = useState<CanvasRenderingContext2D>()
+	const [canvasScale, setCanvasScale] = useState<number>(1)
+
+	const scale = (number: number) => number * canvasScale
+	const scaleSet = <T extends number[]>(...args: T): T => args.map((arg) => arg * canvasScale) as T
 
 	useEffect(() => {
 		const canvasContext = canvasRef.current!.getContext('2d')
@@ -19,17 +24,9 @@ const App = () => {
 
 	useEffect(() => {
 		const canvas = canvasRef.current
-		console.log('canvas', canvasRef, canvas)
 		if (canvas && box) {
-			const { width, height } = box
-			console.log('box', { width, height })
-			// canvas.width = offsetWidth
-			// canvas.height = offsetHeight
-
 			const mapAspectRatio = map.width / map.height
 			const boxAspectRatio = box.width / box.height
-
-			console.log({ mapAspectRatio, boxAspectRatio })
 
 			if (mapAspectRatio > boxAspectRatio) {
 				canvas.width = box.width
@@ -39,26 +36,28 @@ const App = () => {
 				canvas.width = box.height * mapAspectRatio
 			}
 
-			draw()
+			setCanvasScale(canvas.width / map.width)
 		}
-	}, [box])
+	}, [box, context])
 
 	const draw = () => {
 		if (context && canvasRef.current) {
 			context.clearRect(0, 0, canvasRef.current!.width, canvasRef.current!.height)
 
-			const { width, height } = canvasRef.current
-
-			let path1 = new Path2D()
-			path1.rect(width * 0.1, height * 0.4, width * 0.2, width * 0.2)
-
-			let path2 = new Path2D(path1)
-			path2.moveTo(width * 0.8, height * 0.4 + width * 0.1)
-			path2.arc(width * 0.7, height * 0.4 + width * 0.1, width * 0.1, 0, 2 * Math.PI)
-
-			context.stroke(path2)
+			let mapPath = new Path2D()
+			const scaledMapRect = scaleSet(
+				(map.width - field.width) / 2,
+				(map.height - field.height) / 2,
+				field.width,
+				field.height,
+			)
+			console.log('scaledMapRect', scaledMapRect)
+			mapPath.rect(...scaledMapRect)
+			context.stroke(mapPath)
 		}
 	}
+
+	draw()
 
 	return (
 		<div ref={measuredRef} className="root">
