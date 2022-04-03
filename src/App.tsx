b09761830c2bc1d1fb81b0539327b427d452afa3
thomasstock <thomas.stock@aprimo.com>
2022-04-03 13:34:59 +0200

    draw face

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,31 @@ import { useEffect, useRef, useState } from 'react'
 import useCalculateSize from './useCalculateSize'
 
 // https://www.kirupa.com/canvas/canvas_high_dpi_retina.htm
+// https://codepen.io/matt-west/pen/naXPjb
+// https://www.sitepoint.com/building-3d-engine-javascript/
 
 const map = { width: 800, height: 900 }
 const field = { width: 400, height: 500 }
 
 type Vertex3D = { x: number; y: number; z: number }
 type Face = Vertex3D[]
-const createVertex = (x: number, y: number, z = 0) => ({ x, y, z })
+// const createVertex = (x: number, y: number, z = 0) => ({ x, y, z })
 
-const fieldFace: Face = [{ x: }]
+const fieldFace: Face = [
+	{ x: 0, y: 0, z: 0 },
+	{ x: 400, y: 0, z: 0 },
+	{ x: 400, y: 500, z: 0 },
+	{ x: 0, y: 500, z: 0 },
+]
+
+const positionVertex = (startingPoint: Vertex3D, vertex: Vertex3D): Vertex3D => ({
+	x: vertex.x + startingPoint.x,
+	y: vertex.y + startingPoint.y,
+	z: vertex.z + startingPoint.z,
+})
+
+const positionFace = (startingPoint: Vertex3D, face: Face) =>
+	face.map((vertex) => positionVertex(startingPoint, vertex))
 
 const App = () => {
 	const canvasRef = useRef<HTMLCanvasElement | null>(null)
@@ -19,7 +35,6 @@ const App = () => {
 	const [canvasScale, setCanvasScale] = useState<number>(1)
 
 	const scale = (number: number) => number * canvasScale
-	const scaleSet = <T extends number[]>(...args: T): T => args.map((arg) => arg * canvasScale) as T
 
 	useEffect(() => {
 		const canvasContext = canvasRef.current!.getContext('2d')
@@ -50,16 +65,18 @@ const App = () => {
 		if (context && canvasRef.current) {
 			context.clearRect(0, 0, canvasRef.current!.width, canvasRef.current!.height)
 
-			let mapPath = new Path2D()
-			const scaledMapRect = scaleSet(
-				(map.width - field.width) / 2,
-				(map.height - field.height) / 2,
-				field.width,
-				field.height,
-			)
-			console.log('scaledMapRect', scaledMapRect)
-			mapPath.rect(...scaledMapRect)
-			context.stroke(mapPath)
+			const fieldX = fieldFace.reduce((maxX, vertex) => Math.max(maxX, vertex.x), 0)
+			const fieldY = fieldFace.reduce((maxY, vertex) => Math.max(maxY, vertex.y), 0)
+			const fieldStartingPoint = { x: (map.width - fieldX) / 2, y: (map.height - fieldY) / 2, z: 0 }
+
+			const positionedFieldFace = positionFace(fieldStartingPoint, fieldFace)
+
+			drawFace(context, positionedFieldFace, canvasScale)
+
+			// const scaledMapRect = scaleSet(positionedFieldFace.x, positionedFieldFace.y, fieldX, fieldY)
+			// console.log('scaledMapRect', scaledMapRect)
+			// mapPath.rect(...scaledMapRect)
+			// context.stroke(mapPath)
 		}
 	}
 
@@ -70,6 +87,23 @@ const App = () => {
 			<canvas ref={canvasRef}></canvas>
 		</div>
 	)
+}
+
+const drawFace = (context: CanvasRenderingContext2D, face: Face, scale = 1) => {
+	Object.freeze(face)
+
+	const scaleSet = <T extends number[]>(...args: T): T => args.map((arg) => arg * scale) as T
+
+	let path = new Path2D()
+	path.moveTo(...scaleSet(face[0].x, face[0].y))
+
+	const faceReversed = [...face].reverse()
+	// Draw from first point to last point and then all the way back to the first point
+	faceReversed.forEach((vertex) => {
+		path.lineTo(...scaleSet(vertex.x, vertex.y))
+	})
+
+	context.stroke(path)
 }
 
 export default App
